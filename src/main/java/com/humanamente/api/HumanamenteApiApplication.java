bc5465bfa9dd5700fe9f068fee216418011afeee
MUKINH4 <samuelmaragato3@gmail.com>
2025-11-11 08:55:28 -0300

    init: initialized repository

---
+++ src/main/java/com/humanamente/api/HumanamenteApiApplication.java
@@ -0,0 +1,13 @@
+package com.humanamente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanamenteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanamenteApiApplication.class, args);
+	}
+
+}
