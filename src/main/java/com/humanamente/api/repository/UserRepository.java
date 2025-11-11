0cb73df7fe7449381c0aa57d756655c85f31411a
MUKINH4 <samuelmaragato3@gmail.com>
2025-11-11 08:58:52 -0300

    feat: user repository

---
+++ src/main/java/com/humanamente/api/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.humanamente.api.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.humanamente.api.model.User;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+}
